a59b33f8d4c76f101dec40e0ac05cf6cbcdb4f02
Denis Ov <ov4inka1622@gmail.com>
2019-11-18 21:45:00 +0300

    ok

---
+++ app/src/main/java/com/example/myapplication33/ImageSurfaceView.java
@@ -0,0 +1,43 @@
+package com.example.myapplication33;
+
+import android.content.Context;
+
+import android.hardware.Camera;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import java.io.IOException;
+
+public class ImageSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+
+    public ImageSurfaceView(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            this.camera.setPreviewDisplay(holder);
+            this.camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        this.camera.stopPreview();
+        this.camera.release();
+    }
+}
